3e0581f9c447f49508b024352cecc6fcc557208b
Your Name <you@example.com>
2023-04-18 20:48:05 +0300

    'in'

--- forum/templatetags/custom_filters.py
+++ forum/templatetags/custom_filters.py
@@ -6,20 +6,11 @@ register = template.Library()
 
 
 @register.filter()
-def censor(value):
+def no_img(value):
     for i in range(len(value)):
         if value[i:i+10] == '<img src="':
             for n in range(len(value)):
                 if value[n:n+2] == '">':
                     value = value[:i] + value[n+2:]
                     break
-    # value = strip_tags(value)
-    # for i in range(len(value)):
-    #     if value[i:i+4] == '</p>':
-    #         value = value[:i] + value[i+4:]
-    #     if value[i:i+3] == '<p>':
-    #         value = value[:i] + value[i+3:]
-    # for i in range(len(value)):
-    #     if value[i:i+4] == '<br>':
-    #         value = value[:i] + value[i+4:]
     return value
