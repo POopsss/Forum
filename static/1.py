0aca7510d7a603567920479054fb0fe02458f395
Your Name <you@example.com>
2023-04-18 20:39:54 +0300

    'in'

--- static/1.py
+++ static/1.py
@@ -1,4 +1,5 @@
 from forum.models import *
+from allauth.socialaccount.models import SocialApp
 
 c = [
     'Танки',
@@ -16,3 +17,16 @@ c = [
 for i in c:
     Category(category=i).save()
 
+provider = 'google'
+name = 'Google'
+client_id = '630972283500-2sseoi461lk3dm1n19lvscb568i3vh40.apps.googleusercontent.com'
+secret = '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'
+
+SocialApp(provider=provider, name=name,client_id=client_id, secret=secret).save()
+
+provider = 'yandex'
+name = 'Yandex'
+client_id = '9e73c001fdac4203bbeb44f5daa810fb'
+secret = 'f6d36c8af5da472e8e758af816a02bd1'
+
+SocialApp(provider=provider, name=name,client_id=client_id, secret=secret).save()
